21e022ce3791e6da4078f34b90894dc3593b2296
Kamil Lachowicz <kamil109111@gmail.com>
2021-10-18 23:39:19 +0200

    homework week 11

--- src/js/index.js
+++ src/js/index.js
@@ -61,3 +61,20 @@ hamburger.addEventListener('click', () => {
     const nav = document.querySelector('.navigation--js');
     nav.classList.toggle('navigation--open')
 });
+
+fetch('https://api.github.com/users/kamil109111/repos?sort=created&direction=asc')
+.then(resp => resp.json())
+.then(resp => {
+    for (let repo of resp){
+        const {name, html_url} = repo;
+        const repositoryList = document.querySelector('.list--js');
+        const myTemplate = `<li>
+        ${name} <a href="${html_url}" title="link do repozytorium ${name} na
+        githubie">link do githuba</a>
+        </li>`;
+        repositoryList.innerHTML += myTemplate;
+    }    
+})
+.catch(error => {
+    console.log('nie udało się pobrać')
+})
